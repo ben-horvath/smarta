fc0672966bfbbf58379e1a39428c9887729eaba2
Ben <dev.ben.h@gmail.com>
2021-04-15 18:55:19 +0200

    Add asset URL base to webpack config

--- webpack.mix.js
+++ webpack.mix.js
@@ -19,5 +19,20 @@ mix.js('resources/js/app.js', 'public/js').vue()
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
+    const ASSET_URL = process.env.ASSET_URL + "/";
+
+    mix.webpackConfig(webpack => {
+        return {
+            plugins: [
+                new webpack.DefinePlugin({
+                    "process.env.ASSET_PATH": JSON.stringify(ASSET_URL)
+                })
+            ],
+            output: {
+                publicPath: ASSET_URL
+            }
+        }
+    });
+
     mix.version();
 }
