870cd86460c929e1c38c05f4680c25707387acab
Ben <dev.ben.h@gmail.com>
2021-04-15 19:43:27 +0200

    Customize the welcome and dashboard pages

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -8,8 +8,13 @@
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <welcome />
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg flex flex-col justify-center items-center p-6">
+                    <p class="p-6 text-center">How much time do you have right now?</p>
+                    <div class="flex flex-col justify-center items-center">
+                        <button class="block px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 m-2">Less than 30 minutes</button>
+                        <button class="block px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 m-2">30 - 90 minutes</button>
+                        <button class="block px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 m-2">More than 90 minutes</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -18,12 +23,10 @@
 
 <script>
     import AppLayout from '@/Layouts/AppLayout'
-    import Welcome from '@/Jetstream/Welcome'
 
     export default {
         components: {
             AppLayout,
-            Welcome,
         },
     }
 </script>
